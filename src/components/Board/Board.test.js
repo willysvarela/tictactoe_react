e4ba1218af3436ad45465466e5d6a15aa945b5cf
willysv <willysv@motorola.com>
2021-12-16 00:50:53 -0400

    - Added .gitignore - added tests to check first move - beginning of proccess O move function

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,9 +1,47 @@
-import {play, getCircleMove, X, O, _} from './';
+import {play, getCircleMove, X, O, _, getIsFirstMove, boardToArray} from './';
 
-test('return a correct board with O at position [1]', () => {
-    const board = '        x';
-    const newBoard = play(board);
+test('return a correct board with X at position 8 and O at position [1]', () => {
+    const board = '         ';
+    const posX = 8;
+    const newBoard = play(board, posX);
     const expectedBoard = ' o      x';
     expect(newBoard).toBe(expectedBoard);
 });
 
+/*test('return example board', () => {
+    const board = ' xxo  o  ';
+    const posX = 8;
+    const newBoard = play(board, posX);
+    const expectedBoard = 'oxxo  o  ';
+    console.log(newBoard);
+    expect(newBoard).toBe(expectedBoard);
+});*/
+
+
+test('is first move TRUE', () => {
+    const board = ' x       ';
+    const boardList = boardToArray(board);
+    const isFirstMove = getIsFirstMove(boardList);
+    expect(isFirstMove).toBe(true);
+});
+
+
+test('is first move FALSE - 1', () => {
+    const board = ' x o x   ';
+    const boardList = boardToArray(board);
+    const isFirstMove = getIsFirstMove(boardList);
+    expect(isFirstMove).toBe(false);
+});
+
+
+test('is first move FALSE - 2', () => {
+    const board = ' x     xo';
+    const boardList = boardToArray(board);
+    const isFirstMove = getIsFirstMove(boardList);
+    expect(isFirstMove).toBe(false);
+});
+
+/*
+- Board é recebido como string
+- 
+*/
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -6,8 +6,18 @@ export const _ = ' ';
 
 const INITIAL_BOARD = "         ";
 // board parseado em array
-// boardList = ['X', '', '', '', '', '', '', '', ''];
-export const play = board => {
+// boardList = ['X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+export const play = (board, posX) => {
+    const newBoard = playXMove(board, posX);
+    return playCircleMove(newBoard);
+}
+
+export const boardToArray = board => {
+    return board.split('');
+}
+
+export const playCircleMove = (board) => {
     //parseia o board para um array
     const boardList = board.split('');
     // define próx. jogada de O.
@@ -16,17 +26,56 @@ export const play = board => {
     return newBoard.join('');
 }
 
+export const playXMove = (board, pos) => {
+    const boardList = board.split('');
+    const newBoard = getXMove(boardList, pos);
+    return newBoard.join('');
+}
+
+export const getXMove = (boardList, pos) => {
+    return boardList.map((item, i) => i === pos ? X : item);
+}
+
 export const getCircleMove = boardList => {
-    const pos = 1//Math.max(9);
-    return boardList.map((square, i) => i === pos ? O : square);
+    const pos = processCircleMove(boardList);
+    return boardList.map((item, i) => i === pos ? O : item);
+}
+
+const CORNERS = [0, 2, 6, 8];
+const EDGES = [1, 3, 5, 7];
+const CENTER = 4;
+
+export const processCircleMove = boardList => {
+    let pos = 1;
+    //função que retorna a estratégia a ser utilizada, e qual a prox. jogada a ser feita (primeira, segunda, etc.). 
+    const isFirstMove = getIsFirstMove(boardList);
+    if (isFirstMove) {
+        //pegando a posição do X
+        const posX = boardList.findIndex(item => item === X);
+        //primeira jogada no CORNER
+        if (CORNERS.includes(posX)) {
+            return CENTER;
+        }
+        //primeira jogada na BORDA/EDGE
+        if (EDGES.includes(posX)) {
+            
+        } else {
+            //primeira jogada no CENTER
+
+        }
+    }
+    return pos;
+}
+
+export const getIsFirstMove = (boardList) => {
+    return boardList.filter(item => item !== _).length === 1
 }
 
 export const Board = () => {
-
     const [board, setBoard] = useState(INITIAL_BOARD);
     return (
         <div>
-            
+
         </div>
     )
 }
